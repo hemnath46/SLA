cd5579181f246d20099468e608e94303ab1c302d
Hemnath <ahemnath.abhm@gmail.com>
2025-09-03 16:53:46 +0530

    js

---
+++ SLA JS/day1.js
@@ -0,0 +1,40 @@
+let personname='hemnath';
+{
+    console.log(personname)
+    console.log(typeof(personname))
+}
+
+let status=false;
+{
+    console.log(status)
+    console.log(typeof(status))
+}
+
+let name=null;
+{
+    console.log(name)
+    console.log(typeof(name))
+}
+
+let mydeclare;
+{
+    console.log(mydeclare)
+    console.log(typeof(mudeclare))
+}
+
+const course=['java','python','javascript']
+{
+    console.log(course)
+    console.log(typeof(course))
+}
+
+const user={
+    name:'hemnath',
+    age:22,
+    gender:'male',
+    phone:9944403492
+};
+{
+    console.log(user);
+    console.log(typeof(user));
+}
