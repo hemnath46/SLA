bbda5077d2c820e684afb1fd1f5ee4c130fa76f5
hemnath <ahemnath.abhm@gmail.com>
2025-09-09 18:26:02 +0530

    today classes

---
+++ SLA JS/switchstatement.js
@@ -0,0 +1,71 @@
+// switch statement
+let grade1 = 'A';
+let grade2 = 'B';
+let grade3 = 'C';
+let grade4 = 'D';
+switch (grade2) {
+    case (grade2):
+        console.log(grade2)
+        break;
+    // switch stattement
+}
+let marks = 'Good';
+switch (marks) {
+    case 'excellent':
+        console.log('The grade is A ')
+        break;
+    case 'Good':
+        console.log('The grade is B')
+        break;
+    case 'Average':
+        console.log('The grade is C')
+        break;
+    case 'BelowAverage':
+        console.log('The grade is D ')
+        break;
+    default:
+        console.log('invalid marks')
+}
+// switch case
+let day = 'friday';
+let message;
+switch (day) {
+    case 'monday':
+        message = 'first day of week';
+        console.log(message)
+        break;
+
+    case 'tuesday':
+        message = '2nd day of week';
+        console.log(message)
+        break;
+
+    case 'wednesday':
+        message = '3rd day of week';
+        console.log(message)
+        break;
+
+    case 'thursday':
+        message = '4th day of week';
+        console.log(message)
+        break;
+
+    case 'friday':
+        message ='5th day of week';
+        console.log(message)
+        break;
+
+    case 'saturday':
+        message = '6th day of week';
+        console.log(message)
+        break;
+
+    case 'sunday':
+        message = '7th day of week';
+        console.log(message)
+        break;
+
+    default:
+        message = 'no day invaild';
+        console.log(message)
+}
---
+++ SLA JS/test.js
@@ -0,0 +1,89 @@
+// 1.check the password
+let password = 16;
+let PASSWORD = 16;
+if (password == PASSWORD) {
+    console.log('password is correct')
+}
+else {
+    console.log('password is wrong')
+}
+
+// 2. find the exact term='admin' with multiple OR statements
+let A = 'admin';
+let B = 'leader';
+let C = 'admin';
+if (A == B || A == C || B == C) {
+    console.log('admin')
+}
+else {
+    console.log('incorrect')
+}
+
+// 3.print even num
+let number = 10;
+// 0 is a remainder 
+if (number % 2 == 0) {
+    console.log('even num')
+}
+else {
+    console.log('odd num')
+}
+
+// 4.print the given str is num or string.
+let print = '2003';
+let r = 'str';
+if (r == 'str' && print == '2003') {
+    console.log('s')
+}
+else {
+    console.log('h')
+}
+
+// 5.if else elseif statement.
+let first_name = 'hemnath';
+if (first_name == 'joe') {
+    console.log('the first_name is' + ' ' + first_name)
+}
+// in else if we can give many condition
+else if (first_name == 'hem') {
+    console.log('the first_name is' + ' ' + first_name)
+}
+else if (first_name == 'hemnath') {
+    console.log('the first_name is' + ' ' + first_name)
+}
+
+// 6. marks=65
+let mark = 65;
+if (mark >= 70 && mark < 70) {
+    console.log('the student mark is' + '' + mark)
+}
+else if (mark > 80 && mark >= 85) {
+    console.log('B')
+}
+else if (mark <= 65 && mark < 40) {
+    console.log('c')
+}
+
+// looping statement for no
+for (let i = 0; i < 10; i++) {
+    console.log('print i value is' + ' ' + i)
+}
+// for string
+str = 'javascript';
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i])
+}
+
+// homogeneous values
+const course = ['py', 'css', 'html', 'js', 'php', 'beta', 'sql']
+for (let j = 0; j < course.length; j++) {
+    console.log(course[j])
+}
+
+// hetrogeneous ddatatype values
+const hetro = ['bb', 'gt', 16, 11, 'we', 112]
+for (let k = 0; k < hetro.length; k++) {
+    console.log(hetro[k])
+}
+
+
